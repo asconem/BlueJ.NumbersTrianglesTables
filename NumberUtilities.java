6a2bc78e68ec514855c56920fcf78fc1ef284a57
Matthew Ascone <mascone@zipcodes-MacBook-Pro-9.local>
2020-02-05 16:31:31 -0500

    Finished NumberUtilities.

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,58 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+      String a = "";
+      for (int i = 0; i < stop; i++) {
+        a += i;
+      }
+        return a;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+      String b = "";
+      for (int i = start; i < stop; i++) {
+        b += i;
+      }
+        return b;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+      String c = "";
+      for (int i = start; i < stop; i += step) {
+        c += i;
+      }
+        return c;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+      String d = "";
+      for (int i = start; i < stop; i++) {
+        if (i % 2 == 0) {
+          d += i;
+        }
+      }
+        return d;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+      String e = "";
+      for (int i = start; i < stop; i++) {
+        if (i % 2 != 0) {
+          e += i;
+        }
+      }
+        return e;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+      String f = "";
+      for (int i = start; i <= stop; i++) {
+        int exp = (int)Math.pow(i, exponent);
+        f += exp;
+      }
+        return f;
     }
 }
