aa3466973ea65f0d8e205d089f6e2db470611d77
Matthew Ascone <mascone@zipcodes-MacBook-Pro-9.local>
2020-02-05 19:02:06 -0500

    Finished TableUtilities.

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,56 @@
- 
+
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
-    }
-
+      String a = "";
+      for (int row = 1; row <= 5; row++) {
+        for (int col = 1; col <= 5; col++) {
+          if (col*row < 10) {
+            a += "  " + col*row + " |";
+          }
+          else {
+            a += " " + col*row + " |";
+          }
+        }
+        a += "\n";
+      }
+    return a;
+}
     public static String getLargeMultiplicationTable() {
-        return null;
+      String a = "";
+      for (int row = 1; row <= 10; row++) {
+        for (int col = 1; col <= 10; col++) {
+          if (col*row < 10) {
+            a += "  " + col*row + " |";
+          }
+          else if ((col*row >= 10) && (col*row < 100)){
+            a += " " + col*row + " |";
+          }
+          else {
+            a += col*row + " |";
+          }
+        }
+        a += "\n";
+      }
+    return a;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+      String a = "";
+      for (int row = 1; row <= tableSize; row++) {
+        for (int col = 1; col <= tableSize; col++) {
+          if (col*row < 10) {
+            a += "  " + col*row + " |";
+          }
+          else if ((col*row >= 10) && (col*row < 100)){
+            a += " " + col*row + " |";
+          }
+          else {
+            a += col*row + " |";
+          }
+        }
+        a += "\n";
+      }
+    return a;
     }
 }
